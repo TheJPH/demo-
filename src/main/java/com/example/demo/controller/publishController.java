896a089136b1365c1fde62ce047806262844217e
small-figure <727027752@qq.com>
2019-07-15 10:29:35 +0800

    interceptor

--- src/main/java/com/example/demo/controller/publishController.java
+++ src/main/java/com/example/demo/controller/publishController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.mapper.QuestionMapper;
-import com.example.demo.mapper.UserMapper;
+import com.example.demo.mapper.QuestionMapper;;
 import com.example.demo.model.Question;
 import com.example.demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -19,8 +16,6 @@ public class publishController {
 
     @Autowired
     private QuestionMapper questionMapper;
-    @Autowired
-    private UserMapper userMapper;
 
     @GetMapping("/publish")
     public String publish() {
@@ -29,9 +24,9 @@ public class publishController {
 
     @PostMapping("/publish")
     public <request> String doPublish(
-            @RequestParam(value = "title",required = false) String title,
-            @RequestParam(value = "description",required = false) String description,
-            @RequestParam(value = "tag",required = false) String tag,
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "tag", required = false) String tag,
             HttpServletRequest request,
             Model model) {
         model.addAttribute("title", title);
@@ -40,35 +35,22 @@ public class publishController {
         if (title == null || title == "") {
             model.addAttribute("error", "标题不能为空");
             return "publish";
-        } if (description == null || description == "") {
+        }
+        if (description == null || description == "") {
             model.addAttribute("error", "问题补充不能为空");
             return "publish";
-        } if (tag == null || tag == "") {
+        }
+        if (tag == null || tag == "") {
             model.addAttribute("error", "标签不能为空");
             return "publish";
         }
 
-
-
-        User user = null;
-
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0)
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")) {
-                String token = cookie.getValue();
-                user = userMapper.findByToken(token);
-                if (user != null) {
-                    request.getSession().setAttribute("user", user);
-                }
-                break;
-            }
-            if (user == null) {
-                model.addAttribute("error", "用户未登录");
-                return "publish";
-            }
-
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+            return "publish";
         }
+
 
         Question question = new Question();
         question.setTitle(title);
